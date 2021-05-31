c9e8267b913638a355747a32da3fdfddf58ca7ac
uli-oli <ez1ollie@yahoo.com>
2021-05-30 22:38:26 -0700

    Update user_pass.py

--- user_pass.py
+++ user_pass.py
@@ -14,6 +14,10 @@ alt_user = os.getenv('alt_user')
 user_pass_dict[alt_user]=os.getenv('alt_pass')
 user_list.append(alt_user)
 
+iron_user = os.getenv('iron_user')
+user_pass_dict[iron_user]=os.getenv('iron_pass')
+user_list.append(iron_user)
+
 """
 To use this create a file called '.env'
 Format the file like this:
@@ -23,4 +27,7 @@ main_pass = 'password1'
 
 alt_user = 'username2'
 alt_pass = 'password2'
+
+iron_user = 'username3'
+iron_pass = 'password3'
 """
