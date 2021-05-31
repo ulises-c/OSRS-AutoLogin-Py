6005124e85c02cde93ec883b411a46ea4957bd14
uli-oli <ez1ollie@yahoo.com>
2021-05-30 17:16:33 -0700

    Update autoLogger.py
    Opens RuneLite and enters username / password

--- autoLogger.py
+++ autoLogger.py
@@ -2,10 +2,18 @@
 from user_pass import user_pass_dict
 
 import pyautogui as auto
+import os
+import time
 
 username = 'testing1'
 password = 'testing2'
 
+os.system("open /Applications/RuneLite.app")
+
+time.sleep(25)
+
+auto.press('enter')
 auto.write(username)
 auto.press('enter')
 auto.write(password)
+auto.press('enter')
